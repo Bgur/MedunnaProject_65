dffe92ee99868e3c37ac526e11de967a2706a48f
Burhan Gür <burhangr.91@gmail.com>
2022-08-29 22:33:07 +0200

    güncel

--- src/test/java/pages/US015page.java
+++ src/test/java/pages/US015page.java
@@ -48,6 +48,88 @@ public class US015page {
     public WebElement saveButonu;
     @FindBy(xpath = "//div[@role='alert']")
     public WebElement patientKayitOlusturuldu;
+    @FindBy(xpath = "//*[text()='SNN']")
+    public WebElement patientSNN;
+    @FindBy(xpath = "//*[text()='Created Date']")
+    public WebElement patientCreatedDate;
+
+
+    @FindBy(xpath = "//*[text()='856-45-6777']")
+    public WebElement HastaSnnNumarasiKontrol;
+    @FindBy(xpath = "//*[text()='Myrtice']")
+    public WebElement HastaFirstNameKontrol;
+    @FindBy(xpath = "//*[text()='Gaylord']")
+    public WebElement HastaLastNameKontrol;
+    @FindBy(xpath = "//*[text()='30/07/67 03:17']")
+    public WebElement HastaBirthDateKontrol;
+    @FindBy(xpath = "(//*[text()='4165667780'])[3]")
+    public WebElement HastaPhoneNumberKontrol;
+    @FindBy(xpath = "(//*[text()='FEMALE'])[1]")
+    public WebElement HastaGenderKontrol;
+    @FindBy(xpath = "(//*[text()='B-'])[2]")
+    public WebElement HastaBloodGroupKontrol;
+    @FindBy(xpath = "//*[text()='Antalya']")
+    public WebElement HastaAdressKontrol;
+    @FindBy(xpath = "//*[text()='asthma']")
+    public WebElement HastaDescriptionKontrol;
+    @FindBy(xpath = "//*[text()='28/12/21 10:24']")
+    public WebElement HastaCreatedDateKontrol;
+    @FindBy(xpath = "//*[text()='admin']")
+    public WebElement HastaUserKontrol;
+    @FindBy(xpath = "(//*[text()='Turkey'])[2]")
+    public WebElement HastaCountryKontrol;
+    @FindBy(xpath = "//*[text()='Van']")
+    public WebElement HastaStateCityKontrol;
+    @FindBy(xpath = "(//*[text()='Edit'])[1]")
+    public WebElement ilkHastaEditButonu;
+    @FindBy(xpath = "//span[text()='SNN']")
+    public WebElement hastaSNN;
+    @FindBy(xpath = "//input[@name='firstName']")
+    public WebElement hastaFirstName;
+    @FindBy(xpath = "//input[@name='lastName']")
+    public WebElement hastaLastName;
+    @FindBy(xpath = "//input[@name='birthDate']")
+    public WebElement hastaBirthDate;
+    @FindBy(xpath = "//input[@name='phone']")
+    public WebElement hastaPhoneNumber;
+    @FindBy(xpath = "//select[@name='gender']")
+    public WebElement hastaGender;
+    @FindBy(xpath = "//select[@name='bloodGroup']")
+    public WebElement hastaBloodGroup;
+    @FindBy(xpath = "//input[@name='adress']")
+    public WebElement hastaAdress;
+    @FindBy(xpath = "//textarea[@name='description']")
+    public WebElement hastaDescription;
+    @FindBy(xpath = "//span[text()='Created Date']")
+    public WebElement hastaCreatedDate;
+    @FindBy(xpath = "//select[@name='user.id']")
+    public WebElement hastaUser;
+    @FindBy(xpath = "//select[@name='country.id']")
+    public WebElement hastaCountry;
+    @FindBy(xpath = "//*[text()='Save']")
+    public WebElement guncellemeSaveButonu;
+    @FindBy(xpath = "(//*[text()='Appointment'])[1]")
+    public WebElement appointmentButonu;
+    @FindBy(xpath = "(//*[text()='Edit'])[1]")
+    public WebElement doktorAtamaEditButonu;
+    @FindBy(xpath = "//select[@name='physician.id']")
+    public WebElement doktorSecme;
+    @FindBy(xpath = "//*[text='The Appointment is updated with identifier 2301']")
+    public WebElement hastayaDoktorAtandiYazisi;
+
+
+
+
+    @FindBy(xpath = "//select[@name='cstate.id']")
+    public WebElement hastaStateCity;
+    @FindBy(xpath = "//div[@role='alert']")
+    public WebElement hastaBilgisiGuncellendi;
+
+
+
+
+
+
 
 
 
--- src/test/java/stepDefinitions/Ui/US015StepD.java
+++ src/test/java/stepDefinitions/Ui/US015StepD.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import io.cucumber.java.en.*;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import pages.US015page;
@@ -14,6 +15,8 @@ import utilities.ReusableMethods;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class US015StepD {
@@ -23,6 +26,12 @@ public class US015StepD {
     Actions actions=new Actions(Driver.getDriver());
     Faker faker=new Faker();
     Select select;
+    LocalDate localDate=LocalDate.now();
+    LocalDate duzenlenmisLocalDate = localDate.minusDays(10).minusMonths(3).minusYears(30);
+    DateTimeFormatter duzenliDateStart = DateTimeFormatter.ofPattern("dd/MM/yyyy ");
+    String checkInDate = duzenlenmisLocalDate.format(duzenliDateStart);
+    List<String> statesInUs;
+    List<String> theListOfNameOfStatesFromWebTable;
 
     @Given("Admin medunnaUrl sayfasina gider")
     public void admin_medunna_url_sayfasina_gider() {
@@ -87,10 +96,7 @@ public class US015StepD {
 
     @When("Admin Birth date alanina  {string} girer")
     public void admin_birth_date_alanina_girer(String string) {
-        LocalDate localDate=LocalDate.now();
-        LocalDate duzenlenmisLocalDate = localDate.minusDays(10).minusMonths(3).minusYears(30);
-        DateTimeFormatter duzenliDateStart = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String checkInDate = duzenlenmisLocalDate.format(duzenliDateStart);
+
         Us015page.patientBirthDay.sendKeys(checkInDate);
     }
 
@@ -135,7 +141,7 @@ public class US015StepD {
     @When("Admin user alanina  kullanici secer")
     public void admin_user_alanina_kullanici_secer() {
         select=new Select(Us015page.patientUser);
-        select.selectByVisibleText("staff");
+        select.selectByVisibleText("hy");
 
     }
 
@@ -145,8 +151,8 @@ public class US015StepD {
         select.selectByVisibleText("Germany");
     }
 
-    @And("Admin State-City alanina hastanin yasadigi sehri secer")
-    public void adminStateCityAlaninaHastaninYasadigiSehriSecer() {
+    @And("Admin State-City alanini bos birakir")
+    public void adminStateCityAlaniniBosBirakir() {
         Us015page.patientState.click();
     }
 
@@ -154,6 +160,7 @@ public class US015StepD {
     @When("Admin Save butonuna tiklar")
     public void admin_save_butonuna_tiklar() {
         Us015page.saveButonu.click();
+        ReusableMethods.waitFor(4);
     }
 
     @When("Admin A new Patient is created mesajini gorur")
@@ -165,12 +172,279 @@ public class US015StepD {
         System.out.println("alert yazisi :"+ Us015page.patientKayitOlusturuldu.getText());
     }
 
+    @When("Admin 3357 id nolu hastanin SSN numarasi bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_ssn_numarasi_bilgisini_gorur() {
+        ReusableMethods.waitFor(2);
+        String expectedData= "856-45-6777";
+        Assert.assertEquals(expectedData,Us015page.HastaSnnNumarasiKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin First Name bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_first_name_bilgisini_gorur() {
+        ReusableMethods.waitFor(2);
+        String expectedData= "Myrtice";
+        Assert.assertEquals(expectedData,Us015page.HastaFirstNameKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Last Name bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_last_name_bilgisini_gorur() {
+        String expectedData= "Gaylord";
+        Assert.assertEquals(expectedData,Us015page.HastaLastNameKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Birt Date bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_birt_date_bilgisini_gorur() {
+        String expectedData= "30/07/67 03:17";
+        Assert.assertEquals(expectedData,Us015page.HastaBirthDateKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Phone number bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_phone_number_bilgisini_gorur() {
+        String expectedData= "4165667780";
+        Assert.assertEquals(expectedData,Us015page.HastaPhoneNumberKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Gender bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_gender_bilgisini_gorur() {
+        String expectedData= "FEMALE";
+        Assert.assertEquals(expectedData,Us015page.HastaGenderKontrol.getText());
+
+    }
+
+    @When("Admin 3357 id nolu hastanin Blood Group bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_blood_group_bilgisini_gorur() {
+        ReusableMethods.waitFor(2);
+        String expectedData= "B-";
+        Assert.assertEquals(expectedData,Us015page.HastaBloodGroupKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Adress bilgilesini gorur")
+    public void admin_3357_id_nolu_hastanin_adress_bilgilesini_gorur() {
+        ReusableMethods.waitFor(2);
+        String expectedData= "Antalya";
+        Assert.assertEquals(expectedData,Us015page.HastaAdressKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Description bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_description_bilgisini_gorur() {
+
+        String expectedData= "asthma";
+        Assert.assertEquals(expectedData,Us015page.HastaDescriptionKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Created Date bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_created_date_bilgisini_gorur() {
+        String expectedData= "28/12/21 10:24";
+        Assert.assertEquals(expectedData,Us015page.HastaCreatedDateKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin User bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_user_bilgisini_gorur() {
+        String expectedData= "admin";
+        Assert.assertEquals(expectedData,Us015page.HastaUserKontrol.getText());
+
+    }
+    @When("Admin 3357 id nolu hastanin Country bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_country_bilgisini_gorur() {
+        String expectedData= "Turkey";
+        Assert.assertEquals(expectedData,Us015page.HastaCountryKontrol.getText());
+    }
+    @When("Admin 3357 id nolu hastanin State-City bilgisini gorur")
+    public void admin_3357_id_nolu_hastanin_state_city_bilgisini_gorur() {
+       String expectedData= "Van";
+       Assert.assertEquals(expectedData,Us015page.HastaStateCityKontrol.getText());
+    }
+
+    @When("Admin ilk siradaki hastanin bilgilerini guncellemek icin edit butonuna tiklar")
+    public void admin_ilk_siradaki_hastanin_bilgilerini_guncellemek_icin_edit_butonuna_tiklar() {
+        ReusableMethods.waitFor(2);
+        Us015page.ilkHastaEditButonu.click();
+
+    }
+    @When("Admin hastanin bilgilerini guncellemek icin SNN numarasina data girisi yapar")
+    public void admin_hastanin_bilgilerini_guncellemek_icin_snn_numarasina_data_girisi_yapar() {
+        Assert.assertTrue(Us015page.hastaSNN.isDisplayed());
+
+    }
+    @When("Admin hastanin bilgilerini guncellemek icin Created Date bilgisine data girisi yapar")
+    public void admin_hastanin_hastanin_bilgilerini_guncellemek_icin_created_date_bilgisine_data_girisi_yapar() {
+        Assert.assertTrue(Us015page.hastaCreatedDate.isDisplayed());
+
+    }
+    @When("Admin yeni hasta kaydi olustururken hastanin SNN numarasina data girisi yapar")
+    public void admin_yeni_hasta_kaydi_olustururken_hastanin_snn_numarasina_data_girisi_yapar() {
+        Assert.assertTrue(Us015page.patientSNN.isDisplayed());
+
+    }
+    @When("Admin yeni hasta kaydi olustururken hastanin Created Date bilgisine data girisi yapar")
+    public void admin_yeni_hasta_kaydi_olustururken_hastanin_created_date_bilgisine_data_girisi_yapar() {
+        Assert.assertTrue(Us015page.patientCreatedDate.isDisplayed());
+
+    }
+
+    @When("Admin hastanin First Name bilgisine data girisi yapar")
+    public void admin_hastanin_first_name_bilgisine_data_girisi_yapar() {
+        ReusableMethods.waitFor(2);
+        Us015page.hastaFirstName.clear();
+        Us015page.hastaFirstName.sendKeys("Guncel");
+
+    }
+    @When("Admin hastanin Last Name bilgisine data girisi yapar")
+    public void admin_hastanin_last_name_bilgisine_data_girisi_yapar() {
+        Us015page.hastaLastName.clear();
+        Us015page.hastaLastName.sendKeys("Hasta");
+    }
+    @When("Admin hastanin Birth Date bilgisine data girisi yapar")
+    public void admin_hastanin_birth_date_bilgisine_data_girisi_yapar() {
+
+        Us015page.hastaBirthDate.sendKeys(checkInDate);
+    }
+    @When("Admin hastanin Phone Number bilgisine data girisi yapar")
+    public void admin_hastanin_phone_number_bilgisine_data_girisi_yapar() {
+        Us015page.hastaPhoneNumber.clear();
+        Us015page.hastaPhoneNumber.sendKeys("5321234567");
+
+
+    }
+    @When("Admin hastanin Gender bilgisine data girisi yapar")
+    public void admin_hastanin_gender_bilgisine_data_girisi_yapar() {
+        select=new Select(Us015page.hastaGender);
+        select.selectByVisibleText("FEMALE");
+
+    }
+    @When("Admin hastanin Blood Groub bilgisine data girisi yapar")
+    public void admin_hastanin_blood_groub_bilgisine_data_girisi_yapar() {
+        select=new Select(Us015page.hastaBloodGroup);
+        select.selectByVisibleText("B+");
+
+    }
+    @When("Admin hastanin Adress bilgisine data girisi yapar")
+    public void admin_hastanin_adress_bilgisine_data_girisi_yapar() {
+        Us015page.hastaAdress.clear();
+        Us015page.hastaAdress.sendKeys("Antalya");
+
+    }
+    @When("Admin hastanin Description bilgisine data girisi yapar")
+    public void admin_hastanin_description_bilgisine_data_girisi_yapar() {
+        Us015page.hastaDescription.clear();
+        Us015page.hastaDescription.sendKeys("Kanser");
+    }
+
+    @When("Admin hastanin User bilgisine data girisi yapar")
+    public void admin_hastanin_user_bilgisine_data_girisi_yapar() {
+        select=new Select(Us015page.hastaUser);
+        select.selectByVisibleText("hy");
+
+    }
+    @When("Admin hastanin Country bilgisine data girisi yapar")
+    public void admin_hastanin_country_bilgisine_data_girisi_yapar() {
+        select=new Select(Us015page.hastaCountry);
+        select.selectByVisibleText("Turkey");
+
+    }
+    @When("Admin hastanin State-City bilgisine data girisi yapar")
+    public void admin_hastanin_state_city_bilgisine_data_girisi_yapar() {
+
+    }
+
+
+    @And("Admin A Patient is updated mesajini gorur")
+    public void adminANewPatientIsUpdatedMesajiniGorur() {
+        ReusableMethods.waitFor(2);
+        String expectedText="A Patient is updated";
+        String actualText=Us015page.hastaBilgisiGuncellendi.getText();
+        Assert.assertTrue(actualText.contains(expectedText));
+        System.out.println("alert yazisi :"+ Us015page.hastaBilgisiGuncellendi.getText());
+
+
+
+    }
+
+
+
+
+
     @When("Admin sayfayi kapatir")
     public void admin_sayfayi_kapatir() {
         ReusableMethods.waitFor(1);
         Driver.closeDriver();
 
 
+    }
+
+    @When("Admin hastanin ulkesini secmek icin Country butonuna tiklar")
+    public void admin_hastanin_ulkesini_secmek_icin_country_butonuna_tiklar() {
+        Us015page.hastaCountry.click();
+
+    }
+    @When("Admin Country alaninda Turkey olup olmadigini test eder")
+    public void admin_country_alaninda_turkey_olup_olmadigini_test_eder() {
+        ReusableMethods.waitFor(2);
+        Select country = new Select(Us015page.patientCountry);
+        String expectedText = "USA";
+        String actualText;
+
+
+
+    }
+    @When("Admin Country alaninda Turkey secer")
+    public void admin_country_alaninda_turkey_secer() {
+
+    }
+    @When("Admin Country alaninin doldurulmasinin zorunlu oldugunu test eder")
+    public void admin_country_alaninin_doldurulmasinin_zorunlu_oldugunu_test_eder() {
+
+    }
+
+    @When("Admin Items&Titles ikonu altinda yer alan Appointment bolumune tiklar")
+    public void admin_items_titles_ikonu_altinda_yer_alan_appointment_bolumune_tiklar() {
+        Us015page.appointmentButonu.click();
+
+    }
+    @When("Admin acilan sayfada ilk siradaki hastaya doktor atayabilmek icin edit butonuna tiklar")
+    public void admin_acilan_sayfada_ilk_siradaki_hastaya_doktor_atayabilmek_icin_edit_butonuna_tiklar() {
+        ReusableMethods.waitFor(5);
+        Us015page.doktorAtamaEditButonu.click();
+
+
+
+    }
+    @When("Admin Physician alanina hastaya atayacagi doktoru secer")
+    public void admin_physician_alanina_hastaya_atayacagi_doktoru_secer() {
+        ReusableMethods.waitFor(10);
+        select=new Select(Us015page.doktorSecme);
+        select.selectByVisibleText("94207:Ali Can");
+
+
+    }
+
+
+    @And("Admin A Appointment is updated mesajini gorur")
+    public void adminAAppointmentIsUpdatedMesajiniGorur() {
+        ReusableMethods.waitFor(5);
+        System.out.println("alert yazisi"+ Us015page.hastayaDoktorAtandiYazisi.getText());
+   /*
+        String expectedText="The Appointment is updated with identifier 2301";
+      System.out.println("alert yazisi :"+ Us015page.hastayaDoktorAtandiYazisi.getText());
+          String actualText=Us015page.hastayaDoktorAtandiYazisi.getText();
+       Assert.assertTrue(actualText.contains(expectedText));
+
+    */
+
+    }
+
+    @And("Admin ilk siradaki hastayi silmek icin delete butonuna tiklar")
+    public void adminIlkSiradakiHastayiSilmekIcinDeleteButonunaTiklar() {
+
+
+    }
+
+    @And("Admin acilan Confirm Delete Operation uyari mesajinda delete butonuna tiklar")
+    public void adminAcilanConfirmDeleteOperationUyariMesajindaDeleteButonunaTiklar() {
+    }
+
+    @And("Admin hastanin silinip silmedigini kontrol etmek icin A Patient is deleted mesajini gormeli")
+    public void adminHastaninSilinipSilmediginiKontrolEtmekIcinAPatientIsDeletedMesajiniGormeli() {
     }
 
 
